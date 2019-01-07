8b87310cb2cbcd1c64e54a847ca4cd977bc14f53
Kevin Turner <kevinturner@utexas.edu>
2019-01-07 00:57:10 -0600

    Writes initial few test cases for autoordering

--- SherlockTests/SherlockTests.swift
+++ SherlockTests/SherlockTests.swift
@@ -10,25 +10,57 @@ import XCTest
 @testable import Sherlock
 
 class SherlockTests: XCTestCase {
+    var expectation: XCTestExpectation!
+    var fulfilled = false
+    var expected: [serviceType] = [.wikipedia, .google, .facebook, .twitter]
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        // TODO: have way to specify the exact services to include
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    
+    func testAutoOrdering(){
+        // test autoordering
+        expectation = self.expectation(description: "Wait for autoorder")
+        SherlockServiceManager.main.begin(Query: "Xcode")
+        SherlockServiceManager.main.delegate = self
+        self.waitForExpectations(timeout: 2, handler: nil)
+        
+        expectation = self.expectation(description: "Wait for autoorder")
+        SherlockServiceManager.main.begin(Query: "Kevin Turner")
+        expected = [.facebook, .twitter, .google, .wikipedia]
+        fulfilled = false
+        self.waitForExpectations(timeout: 2, handler: nil)
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+}
+
+extension SherlockTests: SherlockServiceManagerDelegate {
+    func resultsChanged(_ services: [SherlockService]) {
+        
+        
+        var match = false
+        for (service, expectedService) in zip(services, expected) {
+            print("services: \(service.type)")
+            if service.type == expectedService {
+                match = true
+            }
         }
+        
+        if !fulfilled && match {
+            expectation.fulfill()
+            fulfilled = true
+        }
+        
     }
-
+    
+    func resultsCleared() {
+        
+    }
+    
 }
